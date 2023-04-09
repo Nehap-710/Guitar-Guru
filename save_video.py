2b9399019058d344df2a2545ae566addde318326
Neha Pattankar <66563258+Nehap-710@users.noreply.github.com>
2023-04-09 19:43:43 +0530

    Update save_video.py

--- save_video.py
+++ save_video.py
@@ -1,14 +1,14 @@
 import pyrebase
 
 config = {
-    'apiKey': "AIzaSyCZiE4vt37uxA_sJrl03KAhk0jYKOtl5sU",
-    'authDomain': "guitarguru-484be.firebaseapp.com",
-    'projectId': "guitarguru-484be",
-    'storageBucket': "guitarguru-484be.appspot.com",
-    'messagingSenderId': "467197811011",
-    'appId': "1:467197811011:web:95286f40bdc0c718c0f7f6",
-    'measurementId': "G-Y74QRV7FFZ",
-    'databaseURL': "https://guitarguru-484be-default-rtdb.asia-southeast1.firebasedatabase.app"
+    'apiKey': "",
+    'authDomain': "",
+    'projectId': "",
+    'storageBucket': "",
+    'messagingSenderId': "",
+    'appId': "",
+    'measurementId': "",
+    'databaseURL': ""
 }
 
 #saving the video in the database and returning the url of the video
